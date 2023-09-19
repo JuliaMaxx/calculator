f4c909980401f8d92f648e5684be4e9fc2f162f5
JuliaMaxy <jjuliamaxxx@gmail.com>
2023-09-19 15:32:41 +0300

    chore: put icons into separate file

--- scripts/sound.js
+++ scripts/sound.js
@@ -10,11 +10,11 @@ function toggleSound () {
   soundSwitch.classList.toggle('on');
   if (soundSwitch.classList.contains('on')){
     buttons.forEach(button => button.addEventListener('click', playSound));
-    soundSwitch.innerHTML = `<img src="icons8-sound-32 (1).png">`;
+    soundSwitch.innerHTML = `<img src="icons/icons8-sound-32 (1).png">`;
   }
   else {
     buttons.forEach(button => button.removeEventListener('click', playSound))
-    soundSwitch.innerHTML = `<img src="icons8-no-sound-32.png">`;
+    soundSwitch.innerHTML = `<img src="icons/icons8-no-sound-32.png">`;
   }
 }
 
